0299669d077f86989344678dee85fb74554c6fdb
Evan Stoddard <evan.stoddard@me.com>
2020-03-23 01:01:17 -0400

    Added examples to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,57 @@
-//
-//  main.cpp
-//  cpp_menu
-//
-//  Created by Evan Stoddard on 3/22/20.
-//  Copyright © 2020 Evan Stoddard. All rights reserved.
-//
 
 #include <iostream>
 #include "menu.h"
 
-void callback(int option) {
-    std::cout << "Selection: " << option << std::endl;
+void globalCallback(int option) {
+    std::cout << "Global Callback | Selection: " << option << std::endl;
+}
+
+void option0() {
+    std::cout << "Item Callback | Selected option 0" << std::endl;
+}
+
+void option1() {
+    std::cout << "Item Callback | Selected option 1" << std::endl;
+}
+
+void option2() {
+    std::cout << "Item Callback | Selected option 2" << std::endl;
 }
 
 int main(int argc, const char * argv[]) {
     
-    Menu menu("Selection an option:", &callback);
+    // Menu with no header or global callback
+    Menu defaultMenu;
+    defaultMenu.addItem("Option 0", &option0);
+    defaultMenu.addItem("Option 1", &option0);
+    defaultMenu.addItem("Option 2", &option0);
     
-    menu.addItem("Option 1");
-    menu.addItem("Option 2");
-    menu.addItem("Option 3");
+    defaultMenu.printMenu();
     
-    menu.printMenu();
+    // Menu with header and no global callback
+    Menu headerMenu("Select an option");
+    headerMenu.addItem("Option 0", &option0);
+    headerMenu.addItem("Option 1", &option0);
+    headerMenu.addItem("Option 2", &option0);
+    
+    headerMenu.printMenu();
+    
+    // Menu with no header and with global callback
+    Menu globalMenu(&globalCallback);
+    globalMenu.addItem("Option 0");
+    globalMenu.addItem("Option 1", &option1); // You can have global callback and individual callbacks
+    globalMenu.addItem("Option 2");
+    
+    globalMenu.printMenu();
+    
+    // Menu with header and global callbacks
+    Menu globalAndHeader("Selection an option:", &globalCallback);
+    
+    globalAndHeader.addItem("Option 0");
+    globalAndHeader.addItem("Option 1", &option1); // You can have global callback and individual callbacks
+    globalAndHeader.addItem("Option 2");
+    
+    globalAndHeader.printMenu();
     
     return 0;
 }
